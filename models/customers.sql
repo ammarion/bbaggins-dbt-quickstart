fa4111b49515bb644edebf1bbe584420ef85bbd8
Ammar Alim <aalim@adobe.com>
2024-07-09 03:34:24 +0000

    all changes

--- models/customers.sql
+++ models/customers.sql
@@ -1,44 +1,44 @@
-{{ config(
-  materialized = 'view'
-) }}
-
 with customers as (
-    select
-        id as customer_id,
-        first_name,
-        last_name
-    from raw.jaffle_shop.customers
+
+    select * from {{ ref('stg_customers') }}
+
 ),
 
 orders as (
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-    from raw.jaffle_shop.orders
+
+    select * from {{ ref('stg_orders') }}
+
 ),
 
 customer_orders as (
+
     select
         customer_id,
+
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
+
     from orders
-    group by customer_id
+
+    group by 1
+
 ),
 
 final as (
+
     select
-        c.customer_id,
-        c.first_name,
-        c.last_name,
-        co.first_order_date,
-        co.most_recent_order_date,
-        coalesce(co.number_of_orders, 0) as number_of_orders
-    from customers c
-    left join customer_orders co on c.customer_id = co.customer_id
+        customers.customer_id,
+        customers.first_name,
+        customers.last_name,
+        customer_orders.first_order_date,
+        customer_orders.most_recent_order_date,
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+
+    from customers
+
+    left join customer_orders using (customer_id)
+
 )
 
-select * from final;
+select * from final
